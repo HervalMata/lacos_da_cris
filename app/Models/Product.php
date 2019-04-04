34582a19be7b4d7ec59ac35bc7cd939e57614f2d
HervalMata <hervalmataamparo@gmail.com>
2019-04-04 00:49:10 -0300

    Criando model Product

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace LacosDaCris\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use Sluggable;
+
+    protected $fillable = ['name', 'description', 'price', 'active'];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+}
