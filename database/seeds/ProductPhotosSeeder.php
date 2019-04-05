95ce4ccd4b09c575efa0bc6d32c20cf7844f6e4f
HervalMata <hervalmataamparo@gmail.com>
2019-04-05 17:00:52 -0300

    Criando registro das imagens

--- database/seeds/ProductPhotosSeeder.php
+++ database/seeds/ProductPhotosSeeder.php
@@ -17,8 +17,9 @@ class ProductPhotosSeeder extends Seeder
         /** @var Collection $products */
         $products = Product::all();
         $this->deleteAllPhotosInProductsPath();
+        $self = $this;
         $products->each(function ($product) {
-
+            self::createPhotoDir($product);
         });
     }
 
@@ -26,5 +27,26 @@ class ProductPhotosSeeder extends Seeder
     {
         $path = ProductPhoto::PRODUCTS_PATH;
         \File::deleteDirectory(storage_path($path), true);
+    }
+
+    private function createPhotoDir(Product $product)
+    {
+        $path = ProductPhoto::photosPath($product->id);
+        \File::makeDirectory($path, 0777, true);
+    }
+
+    private function createPhotosModels(Product $product)
+    {
+        foreach (range(1, 5) as $v) {
+            $this->createPhotoModel($product);
+        }
+    }
+
+    private function createPhotoModel(Product $product)
+    {
+        ProductPhoto::create([
+            'product_id' => $product->id,
+            'file_name' => 'imagem.jpg'
+        ]);
     }
 }
