749dbb0051a77f6e80a0504c3fab93e256365ed1
HervalMata <hervalmataamparo@gmail.com>
2019-04-06 15:38:16 -0300

    Criando usuários falsos

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use LacosDaCris\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class, 1)->create([
+            'email' => 'admin@user.com'
+        ]);
+
+        factory(User::class, 50)->create();
+    }
+}
