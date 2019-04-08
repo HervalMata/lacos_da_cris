f29f6bac5b62fc67caad19ef86e7c4f01a4ec13b
HervalMata <hervalmataamparo@gmail.com>
2019-04-08 18:54:30 -0300

    Metodo AJAX para listagem de categorias

--- angular-app/src/app/services/http/category-http.service.ts
+++ angular-app/src/app/services/http/category-http.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CategoryHttpService {
 
-  constructor() { }
+    constructor(private http: HttpClient) {
+    }
+
+    list() {
+        const token = window.localStorage.getItem('token');
+        return this.http.get<{ data: Array<{ id: number, name: string, active: boolean, created_at: { date: string } }> }>('http://localhost:8000/api/categories', {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+    }
+
+    get() {
+
+    }
+
+    create() {
+
+    }
+
+    update() {
+
+    }
+
+    destroy() {
+
+    }
 }
