b16dbcf17a098892898789720c329b3d9f39c8a4
HervalMata <hervalmataamparo@gmail.com>
2019-04-10 12:24:21 -0300

    Melhoria na renovacao de token

--- angular-app/src/app/services/auth.service.ts
+++ angular-app/src/app/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {tap} from "rxjs/operators";
@@ -8,55 +8,55 @@ import {JwtHelperService} from '@auth0/angular-jwt';
 const TOKEN_KEY = 'lacos_da_cris_token';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-  me: User = null;
+    me: User = null;
 
-  constructor(private http: HttpClient) {
-    const token = this.getToken();
-    this.setUserFromToken(token);
-  }
+    constructor(private http: HttpClient) {
+        const token = this.getToken();
+        this.setUserFromToken(token);
+    }
 
-  login(user: {email: string, password: string}) : Observable<{token: string}> {
-      return this.http.post<{token: string}>('http://localhost:8000/api/login', user)
-          .pipe(
-              tap(response => {
-                  this.setToken(response.token);
-              })
-          );
-  }
+    login(user: { email: string, password: string }): Observable<{ token: string }> {
+        return this.http.post<{ token: string }>('http://localhost:8000/api/login', user)
+            .pipe(
+                tap(response => {
+                    this.setToken(response.token);
+                })
+            );
+    }
 
-  setToken(token: string) {
-    this.setUserFromToken(token);
-    token ? window.localStorage.setItem(TOKEN_KEY, token) : window.localStorage.removeItem(TOKEN_KEY);
-  }
+    setToken(token: string) {
+        this.setUserFromToken(token);
+        token ? window.localStorage.setItem(TOKEN_KEY, token) : window.localStorage.removeItem(TOKEN_KEY);
+    }
 
-  getToken() : string | null {
-      return window.localStorage.getItem(TOKEN_KEY);
-  }
+    getToken(): string | null {
+        return window.localStorage.getItem(TOKEN_KEY);
+    }
 
-  private setUserFromToken(token: string) {
-    const decodedToken = new JwtHelperService().decodeToken(token);
-    this.me = decodedToken ? {
-      id: decodedToken.sub,
-      name: decodedToken.name,
-      email: decodedToken.email,
-    } : null;
-  }
+    private setUserFromToken(token: string) {
+        const decodedToken = new JwtHelperService().decodeToken(token);
+        this.me = decodedToken ? {
+            id: decodedToken.sub,
+            name: decodedToken.name,
+            email: decodedToken.email,
+        } : null;
+    }
 
-  isAuth() : boolean {
-      const token = this.getToken();
-      return !new JwtHelperService().isTokenExpired(token, 30);
-  }
+    isAuth(): boolean {
+        const token = this.getToken();
+        return !new JwtHelperService().isTokenExpired(token, 30);
+    }
 
-  logout() : Observable<any> {
-      return this.http.post<{token: string}>('http://localhost:8000/api/logout', {})
-          .pipe(
-              tap(() => {
-                  this.setToken(null);
-              })
-          );
-  }
+    logout(): Observable<any> {
+        return this.http.post<{ token: string }>('http://localhost:8000/api/logout', {})
+            .pipe(
+                tap(() => {
+                    this.setToken(null);
+                })
+            );
+    }
 }
--- angular-app/src/app/services/refresh-token-interceptor.service.ts
+++ angular-app/src/app/services/refresh-token-interceptor.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
     HttpErrorResponse,
     HttpEvent,
     HttpHandler,
     HttpInterceptor,
     HttpRequest,
-    HttpResponse
+    HttpResponseBase
 } from "@angular/common/http";
 import {Observable} from "rxjs";
 import {tap} from "rxjs/operators";
@@ -13,41 +13,43 @@ import {AuthService} from "./auth.service";
 import {Router} from "@angular/router";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class RefreshTokenInterceptorService implements HttpInterceptor{
+export class RefreshTokenInterceptorService implements HttpInterceptor {
 
-  constructor(
-      private authService: AuthService,
-      private router: Router
-  ) { }
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    ) {
+    }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next
             .handle(req)
             .pipe(
                 tap((event: HttpEvent<any>) => {
-                  this.setNewTokenIfResponseValid(event);
-                }, (eventError:HttpEvent<any>) => {
-                  this.redirectToLoginIfUnauthenticated(eventError);
+                    this.setNewTokenIfResponseValid(event);
+                }, (eventError: HttpEvent<any>) => {
+                    this.setNewTokenIfResponseValid(eventError);
+                    this.redirectToLoginIfUnauthenticated(eventError);
                 })
             );
     }
 
-    private redirectToLoginIfUnauthenticated(eventError:HttpEvent<any>) {
-      if (eventError instanceof HttpErrorResponse && eventError.status == 401) {
-        this.authService.setToken(null);
-        this.router.navigate(['login']);
-      }
+    private redirectToLoginIfUnauthenticated(eventError: HttpEvent<any>) {
+        if (eventError instanceof HttpErrorResponse && eventError.status == 401) {
+            this.authService.setToken(null);
+            this.router.navigate(['login']);
+        }
     }
 
     private setNewTokenIfResponseValid(event: HttpEvent<any>) {
-      if (event instanceof HttpResponse) {
-        const authorizationHeader = event.headers.get('authorization');
-        if (authorizationHeader) {
-          const token = authorizationHeader.split(' ')[1];
-          this.authService.setToken(token);
+        if (event instanceof HttpResponseBase) {
+            const authorizationHeader = event.headers.get('authorization');
+            if (authorizationHeader) {
+                const token = authorizationHeader.split(' ')[1];
+                this.authService.setToken(token);
+            }
         }
-      }
     }
 }
