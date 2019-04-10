e443ce5e832f383500891d44d513b4bc7b0803da
HervalMata <hervalmataamparo@gmail.com>
2019-04-10 17:26:04 -0300

    Configuracao de ambiente com controle de versao

--- angular-app/src/app/services/http/product-category-http.service.ts
+++ angular-app/src/app/services/http/product-category-http.service.ts
@@ -4,38 +4,32 @@ import {ProductCategory} from "../../model";
 import {HttpClient} from "@angular/common/http";
 import {map} from "rxjs/operators";
 import {environment} from "../../../environments/environment";
+import {AuthService} from "../auth.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProductCategoryHttpService {
 
-    constructor(private http: HttpClient) {
+    constructor(
+        private http: HttpClient,
+        private authService: AuthService
+    ) {
     }
 
     list(productId: number): Observable<ProductCategory> {
-        const token = window.localStorage.getItem('token');
         return this.http.get<{ data: ProductCategory }>
-        (this.getBaseUrl(productId), {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        })
+        (this.getBaseUrl(productId))
             .pipe(map(response => response.data))
     }
 
     create(productId: number, categoriesId: number[]): Observable<ProductCategory> {
-        const token = window.localStorage.getItem('token');
         return this.http.post<{ data: ProductCategory }>
-        (this.getBaseUrl(productId), {categories: categoriesId}, {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        })
+        (this.getBaseUrl(productId), {categories: categoriesId})
             .pipe(map(response => response.data))
     }
 
-    private getBaseUrl(productId: number, categoryId: number = null) : string {
+    private getBaseUrl(productId: number, categoryId: number = null): string {
         let baseUrl = `${environment.api.url}/products/${productId}/categories`;
         if (categoryId) {
             baseUrl += `/${categoryId}`;
