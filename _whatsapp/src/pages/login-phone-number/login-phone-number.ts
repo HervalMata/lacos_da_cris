5e02ac9f317def5294c255108dfe60e34efaa40b
HervalMata <hervalmataamparo@gmail.com>
2019-04-16 18:11:55 -0300

    Configuracao do metodo de autenticacao por telefone

--- _whatsapp/src/pages/login-phone-number/login-phone-number.ts
+++ _whatsapp/src/pages/login-phone-number/login-phone-number.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import * as firebase from 'firebase';
+import * as firebaseui from 'firebaseui';
+import firebaseConfig from '../../app/firebase-config';
 
 /**
  * Generated class for the LoginPhoneNumberPage page.
@@ -15,11 +18,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPhoneNumberPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+      public navCtrl: NavController,
+      public navParams: NavParams
+  ) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPhoneNumberPage');
+    firebase.initializeApp(firebaseConfig);
+    const uiConfig = {
+      signInOptions: [
+          firebase.auth.PhoneAuthProvider.PROVIDER_ID
+      ]
+    }
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start('#firebaseui', uiConfig);
   }
 
 }
