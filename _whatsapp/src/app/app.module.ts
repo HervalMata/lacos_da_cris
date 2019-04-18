b760f121d52bcca1ce7e2a53028e15dc9f5b0617
HervalMata <hervalmataamparo@gmail.com>
2019-04-18 16:18:40 -0300

    Criando pagina para a criacao de cliente - IONIC

--- _whatsapp/src/app/app.module.ts
+++ _whatsapp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {ResetPhoneNumberPage} from "../pages/reset-phone-number/reset-phone-numb
 import { FirebaseAuthProvider } from '../providers/auth/firebase-auth';
 import {HttpClientModule} from "@angular/common/http";
 import {MainPage} from "../pages/main/main";
+import {CustomerCreatePage} from "../pages/customer-create/customer-create";
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import {MainPage} from "../pages/main/main";
     LoginOptionsPage,
     LoginPhoneNumberPage,
     ResetPhoneNumberPage,
+    CustomerCreatePage,
     MainPage
   ],
   imports: [
@@ -38,6 +40,7 @@ import {MainPage} from "../pages/main/main";
     LoginOptionsPage,
     LoginPhoneNumberPage,
     ResetPhoneNumberPage,
+    CustomerCreatePage,
     MainPage
   ],
   providers: [
