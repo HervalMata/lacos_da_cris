3f1a33f5654a02f6c61916afaccc10f003856fbb
HervalMata <hervalmataamparo@gmail.com>
2019-04-17 21:41:17 -0300

    Finalizando metodo para criacao de cliente

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -1,5 +1,5 @@
 <?php
-
+declare(strict_types=1);
 namespace LacosDaCris\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -13,6 +13,21 @@ class UserProfile extends Model
     const USER_PHOTP_PATH = self::BASE_PATH . '/' . self::DIR_USER_PHOTO;
 
     protected $fillable = ['photo', 'phone_number'];
+
+    public static function saveProfile(User $user, array $data) : UserProfile
+    {
+        $data['photo'] = UserProfile::getPhotoHashName($data['photo']);
+        $user
+            ->profile
+            ->fill($data)
+            ->save();
+        return $user->profile;
+    }
+
+    private static function getPhotoHashName(UploadedFile $photo = null)
+    {
+        return $photo ? $photo->hashName() : null;
+    }
 
     /**
      * @return string
@@ -42,6 +57,18 @@ class UserProfile extends Model
         }
         $dir = self::photosDir();
         $photo->store($dir, ['disk' => 'public']);
+    }
+
+    public static function deleteFile(UploadedFile $photo = null)
+    {
+        if (!$photo) {
+            return;
+        }
+        $path = self::photosPath();
+        $photoPath = "{$path}/{$photo->hashName()}";
+        if (file_exists($photoPath)) {
+            \File::delete($photoPath);
+        }
     }
 
     /**
