521afeaad72797a2409c0c894dc39ca8493e85df
HervalMata <hervalmataamparo@gmail.com>
2019-04-18 13:17:44 -0300

    Interface para remover foto do usuario

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -27,7 +27,7 @@ class CorsMiddleware
 
         if ($request->is('api/*')) {
             if (isset($allowOrigin)) {
-                header('Access-Control-Allow-Origin: $allowOrigin');
+                header("Access-Control-Allow-Origin: $allowOrigin");
             }
             header('Access-Control-Allow-Headers: Content-Type, Authorization');
             header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE');
