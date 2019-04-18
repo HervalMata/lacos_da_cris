9551d77c3c5f33d9f137974e321ae43091f81970
HervalMata <hervalmataamparo@gmail.com>
2019-04-17 21:03:54 -0300

    Protegendo API administrativa

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,6 +19,11 @@ class UsersTableSeeder extends Seeder
             $user->profile->save();
         });
 
+        factory(User::class, 1)->create([
+            'email' => 'cliente@user.com',
+            'role' => User::ROLE_CUSTOMER
+        ]);
+
         factory(User::class, 50)->create([
             'role' => User::ROLE_CUSTOMER
         ]);
