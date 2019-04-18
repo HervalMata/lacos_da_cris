58754e0d4ecb7365dfeec8fc0f0135203203a192
HervalMata <hervalmataamparo@gmail.com>
2019-04-18 13:40:22 -0300

    Integracao do Firebase no Angular

---
+++ angular-app/src/app/firebase-config.ts
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAdVdcGjO9G8MhO1zKi0C3hPOO5oLv9oPo",
+    authDomain: "lacos-da-cris.firebaseapp.com",
+    databaseURL: "https://lacos-da-cris.firebaseio.com",
+    projectId: "lacos-da-cris",
+    storageBucket: "lacos-da-cris.appspot.com",
+    messagingSenderId: "732551708033"
+
+};
