41d605a8b7772fa528ae6e9ea1967be732ed6466
HervalMata <hervalmataamparo@gmail.com>
2019-04-18 02:02:51 -0300

    Melhoria na Seeder de Usuarios

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,7 +1,10 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Illuminate\Http\UploadedFile;
 use LacosDaCris\Models\User;
+use LacosDaCris\Models\UserProfile;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,21 +15,43 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        \File::deleteDirectory(UserProfile::photosPath(), true);
         factory(User::class, 1)->create([
             'email' => 'admin@user.com',
             'role' => User::ROLE_SELLER
         ])->each(function ($user) {
-            $user->profile->phone_number = '+16505551234';
-            $user->profile->save();
+            Model::reguard();
+            $user->updateWithProfile([
+                'phone_number' => '+16505551234',
+                'photo' => $this->getAdminPhoto()
+            ]);
+            Model::unguard();
         });
 
         factory(User::class, 1)->create([
             'email' => 'cliente@user.com',
             'role' => User::ROLE_CUSTOMER
-        ]);
+        ])->each(function ($user) {
+            Model::reguard();
+            $user->updateWithProfile([
+                'phone_number' => '+16505551235'
+            ]);
+            Model::unguard();
+        });
 
         factory(User::class, 50)->create([
             'role' => User::ROLE_CUSTOMER
         ]);
+    }
+
+    /**
+     * @return UploadedFile
+     */
+    public function getAdminPhoto()
+    {
+        return new UploadedFile(
+            storage_path('app/faker/users/1624_mod.png'),
+            str_random(16) . '.jpg'
+        );
     }
 }
