2268610574dc58d9d00bbc303c643dda473cc512
HervalMata <hervalmataamparo@gmail.com>
2019-04-19 01:48:42 -0300

    Iniciando conceito de grupos de chat

---
+++ app/Http/Controllers/Api/ChatGroupController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace LacosDaCris\Http\Controllers\Api;
+
+use LacosDaCris\Models\ChatGroup;
+use Illuminate\Http\Request;
+
+class ChatGroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \LacosDaCris\Models\ChatGroup  $chatGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ChatGroup $chatGroup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \LacosDaCris\Models\ChatGroup  $chatGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ChatGroup $chatGroup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \LacosDaCris\Models\ChatGroup  $chatGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ChatGroup $chatGroup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \LacosDaCris\Models\ChatGroup  $chatGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ChatGroup $chatGroup)
+    {
+        //
+    }
+}
