20ee8918fe4d1a2f9512f6ae01e80477c49be5cc
HervalMata <hervalmataamparo@gmail.com>
2019-04-22 13:35:14 -0300

    Iniciando captura de usuario da mensagem

--- _whatsapp/src/pages/chat_messages/chat-messages/chat-messages.ts
+++ _whatsapp/src/pages/chat_messages/chat-messages/chat-messages.ts
@@ -28,7 +28,12 @@ export class ChatMessagesPage {
         const database = this.firebaseAuth.firebase.database();
         database.ref('chat_groups/1/messages').on('child_added', (data) => {
             const message = data.val() as ChatGroup;
-            this.messages.push(message);
+
+            database.ref(`users/${message.user_id}`).on('value', (data) => {
+                message.user = data.val();
+                console.log(message);
+                this.messages.push(message);
+            })
         });
     }
 
