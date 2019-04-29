8539c4bec65e07640ba903c7aa15d9042c348f17
HervalMata <hervalmataamparo@gmail.com>
2019-04-29 02:25:49 -0300

    Organizando geracao de mensagens falsas

---
+++ database/seeds/ChatMessagesLargeFbSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use LacosDaCris\Models\ChatGroup;
+
+class ChatMessagesLargeFbSeeder extends ChatMessagesFbSeeder
+{
+    protected $numMessages = 100;
+
+
+    /**
+     * @return \Illuminate\Support\Collection|ChatGroup[]
+     */
+    protected function getChatGroups()
+    {
+
+        return ChatGroup::whereId(1)->get();
+    }
+
+}
